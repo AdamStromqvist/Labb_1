c1575db1bf9e4d58b2bb717f618a93a8d89a9b7b
lennartAlvin <158577531+lennartAlvin@users.noreply.github.com>
2024-02-02 14:11:32 +0100

    Update DB.cs

--- OOSU2_Laboration1_AAAV/DataLayer/DB.cs
+++ OOSU2_Laboration1_AAAV/DataLayer/DB.cs
@@ -8,15 +8,15 @@ using System.Threading.Tasks;
 namespace OOSU2_Laboration1_AAAV.DataLayer
 {
     //-------------------------------------------
-    //Database, nothing mroe to say?!?
+    //Database
     //-------------------------------------------
     public class DB
     {
-        public List<Patient> Patients { get; set; }
-        public List<DoctorAppointment> DoctorAppointments { get; set; }
-        public List<Diagnosis> Diagnoses { get; set; }
-        public List<MedicinePrescription> MedicinePrescriptions { get; set; }
-
+        public List<Patient> Patients { get; set; } // Represents list for patients
+        public List<DoctorAppointment> DoctorAppointments { get; set; } // Represents list for Doctorappointments
+        public List<Diagnosis> Diagnoses { get; set; } // Represents a list for Diagnoses
+        public List<MedicinePrescription> MedicinePrescriptions { get; set; } // Represents a list for MedicinePrescriptions
+        // Missing references but is an association line between UnitofWork and the database
         public DataLayer.UnitOfWork UnitOfWork
         {
             get => default;
@@ -24,7 +24,7 @@ namespace OOSU2_Laboration1_AAAV.DataLayer
             {
             }
         }
-
+        // Constructor for Database
         public DB()
         {
             Patients = new List<Patient>();
