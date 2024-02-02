2fcba84cccfbedaceeb2a7d793b3512b0819ae46
AlexNobel1 <124204079+AlexNobel1@users.noreply.github.com>
2024-02-02 14:11:02 +0100

    Update Program.cs

--- OOSU2_Laboration1_AAAV/PresentationLayer/Program.cs
+++ OOSU2_Laboration1_AAAV/PresentationLayer/Program.cs
@@ -11,10 +11,6 @@ namespace OOSU2_Laboration1_AAAV.PresentationLayer
 {
     //-------------------------------------------------
     //User interface
-    //Hello from git
-    //test igen
-
-    //jadajada
     //-------------------------------------------------
     class Program
     {
