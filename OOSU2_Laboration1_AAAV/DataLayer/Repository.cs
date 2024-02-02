2f1131e68b2abe12c0dce8e151abf1457f88eac2
lennartAlvin <158577531+lennartAlvin@users.noreply.github.com>
2024-02-02 14:32:28 +0100

    Update Repository.cs

--- OOSU2_Laboration1_AAAV/DataLayer/Repository.cs
+++ OOSU2_Laboration1_AAAV/DataLayer/Repository.cs
@@ -10,9 +10,9 @@ namespace OOSU2_Laboration1_AAAV.DataLayer
     //Generic Repository which means it connects to all enteties so you dont need make one repository per entity!
     //-----------------------------------------------------------------------------------------------------------------------
     public class Repository<T> where T : class
-    {
+    {    // Private list to store data entities.
         private readonly List<T> data;
-
+        //  Constructor to give the repository a list of data entities.
         public Repository(List<T> data)
         {
             this.data = data;
@@ -25,32 +25,32 @@ namespace OOSU2_Laboration1_AAAV.DataLayer
             {
             }
         }
-
+        // Method to add a new entity to the repository.
         public void Add(T entity)
         {
             data.Add(entity);
         }
-
+        // Method to remove an entity from the repository
         public bool Remove(T entity)
         {
             return data.Remove(entity);
         }
-
+        // Method to find entities based on a specified condition
         public IEnumerable<T> Find(Func<T, bool> predicate)
         {
             return data.Where(predicate);
         }
-
+        // Method to find the first entity that matches a specified condtion or a null value.
         public T FirstOrDefault(Func<T, bool> predicate)
         {
             return data.FirstOrDefault(predicate);
         }
-
+        // Method to check if the repository is empty.
         public bool IsEmpty()
         {
             return data.Count == 0;
         }
-
+        // Method to get the count of entities in the repository.
         public int Count()
         {
             return data.Count();
